dcf056973a69897e145a657c6f8ab2b71b7d52a0
Samik <samiksaha88@gmail.com>
2015-07-11 19:31:24 +0530

    Put back support for play.spotify.com along with player.spotify.com

--- glyrics-ext/spotify/extract_track_info.js
+++ glyrics-ext/spotify/extract_track_info.js
@@ -1,13 +1,21 @@
-//Get song information from play.spotify.com page
-function fetchTrackInfo(){
-  songName = '';
-  album = '';
-  firstArtist = '';
-  artists = '';
+//Get song information from Spotify
+function fetchTrackInfo() {
+    window.songName = '';
+    window.album = '';
+    window.firstArtist = '';
+    window.artists = '';
 
-  trackName = $('#app-player').contents().find('#track-name a:first').text().trim();
-  trackArtist = $('#app-player').contents().find('#track-artist a:first').text().trim();
-  
-  songName = trackName;
-  firstArtist = trackArtist;
+    var trackName, trackArtist;
+
+    if (window.location.hostname == "play.spotify.com") {
+        trackName = $('#app-player').contents().find('#track-name a:first').text().trim();
+        trackArtist = $('#app-player').contents().find('#track-artist a:first').text().trim();
+    }
+    else {
+        trackName = $('#main').contents().find('#view-now-playing .track span:first').text().trim();
+        trackArtist = $('#main').contents().find('#view-now-playing .artist span:first').text().trim();
+    }
+
+    window.songName = trackName;
+    window.firstArtist = trackArtist;
 }

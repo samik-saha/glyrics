e766dcd3bcf7a48a8e5f7f0171733af27023c8e8
Samik Saha <samiksaha88@gmail.com>
2016-09-03 22:25:09 +0530

    Fixed Gaana UI changes

--- glyrics-ext/gaana/extract_track_info.js
+++ glyrics-ext/gaana/extract_track_info.js
@@ -1,42 +1,21 @@
 //Get song information from Gaana.com page
 function fetchTrackInfo(){
-  var prevSongName=songName;
-  songName = '';
-  album = '';
-  firstArtist = '';
-  artists = '';
+  var prevSongName=window.songName;
+  window.songName = '';
+  window.album = '';
+  window.artists = '';
 
-  div_trackInfo = document.getElementById('trackInfo');
-  
-  if (!div_trackInfo || div_trackInfo.children.length === 0){
-      return;
-  }
-  
-
-  //For Radio Mirchi
-  songNames = div_trackInfo.getElementsByClassName("songName");
-  if (songNames.length > 0){
-      songName = songNames[0].innerText.trim();
-      albumElement = div_trackInfo.getElementsByClassName("albumNamePl");
-      album = (albumElement.length > 0)?albumElement[0].innerText.trim():"";
-      tx=$("#tx");
-      if(tx.length === 0) return;
-      firstArtist = tx.find("span").eq(1).find("a").eq(0).text().trim();
-  }else {
-      //Gaana.com default
-      tx=$("#tx");
-      if(tx.length === 0) return;
-      songName = tx.get(0).firstChild.nodeValue.trim();
-      album = tx.find("span").eq(0).find("a").text().trim();
-      firstArtist = tx.find("span").eq(1).find("a").eq(0).text().trim();
-  }
+  songName = $('#stitle').first().text().trim()
+  album = $('#atitle').first().text().trim()
 
   /* Ignore Gaana Promotional */
-  if (songName === "Gaana Promotional"){songName = prevSongName;}
+  if (songName === "Gaana Promotional"){
+	  songName = prevSongName;
+  }
   
   //if song changed and artist is null
-  // 12/27/2013 -- This might no longer being required as artist name is now shown in the player
-  if (songName !== prevSongName && !firstArtist){
+  if (window.songName !== prevSongName){
+	  window.firstArtist = '';
       //Look for artist elsewhere in the page
       extractTracksFromPage();
   }
@@ -46,8 +25,6 @@ function fetchTrackInfo(){
 /* 
 Although artist name is not displayed, it might be available elsewhere in the page
 This function finds all songs listed in the page and extracts the artist for the current song.
-
-12/27/2013 -- This might no longer be required as artist name is now shown in the player
 */
 function extractTracksFromPage(){
     var allSpans = document.getElementsByTagName("span");
@@ -60,9 +37,9 @@ function extractTracksFromPage(){
     }
     
     for (i=0;i<songJSON.length;i++){
-        obj = eval("("+songJSON[i]+")");
-        if (obj.title.trim() === songName.trim()){
-            firstArtist = obj.artist.substring(0,obj.artist.indexOf("###"));
+        var obj = eval("("+songJSON[i]+")");
+        if (obj.title.trim() == window.songName){
+            window.firstArtist = obj.artist.substring(0,obj.artist.indexOf("###"));
         }
         //console.log(obj.title+":"+obj.artist);
     }

2ae84131bd75f10c8506d0035dc84ee1e6a70c34
Samik Saha <samiksaha88@gmail.com>
2016-09-17 00:04:48 +0530

    added PowerShell script for compiling

---
+++ compile.ps1
@@ -0,0 +1,15 @@
+﻿$files = Get-ChildItem -Path "glyrics-ext" -Recurse|Where-Object{!($_.PSIsContainer)}
+for ($i=0; $i -lt $files.Count; $i++) {
+    $infile = Resolve-Path -Path $files[$i].FullName -Relative
+    
+    $outfile = "publish\" + $infile
+    if( $infile -match "\.js$" -and $infile -notmatch "\.min.js$"){
+        echo "Compiling $infile to $outfile"
+        java -jar closure-compiler.jar --compilation_level SIMPLE --js $infile --js_output_file $outfile
+    }
+    else{
+        echo "Copying $infile to $outfile"
+        cp $infile $outfile
+    }
+}
+echo "Done!"

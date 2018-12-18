648f3252897428ad3623a298fea921543203298c
Samik Saha <3612424+samik-saha@users.noreply.github.com>
2018-12-19 02:05:08 +0530

    added test case for soundcloud track info extraction

--- test/glyrics-ext/testTrackExtraction.js
+++ test/glyrics-ext/testTrackExtraction.js
@@ -30,6 +30,35 @@ describe('track details extraction', function(){
             expect(artist).to.equal('Maroon 5');
         });
     });
+
+    describe('soundcloud',function(){
+        var trackTitle;
+        var artist;
+        var album;
+        before(async function(){
+            this.timeout(15000);
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto('https://soundcloud.com/postmalone/candy-paint', {waitUntil: "networkidle2"});
+            //Inject JQuery
+            await page.addScriptTag({path:"glyrics-ext/jquery-3.3.1.min.js"});
+            //Inject js file
+            await page.addScriptTag({path:"glyrics-ext/soundcloud/extract_track_info.js"});
+            //await page.click('button[play-song]');
+            //await sleep(2000);
+            trackTitle = await page.evaluate('fetchTrackInfo();window.songName');
+            album = await page.evaluate('window.album');
+            artist = await page.evaluate('window.firstArtist');
+            await browser.close();
+        });
+        it('track', async function(){
+            expect(trackTitle).to.equal('Candy Paint');
+        });
+
+        it('artist', async function(){
+            expect(artist).to.equal('Post Malone');
+        });
+    });
 });
 
 function sleep(ms) {
